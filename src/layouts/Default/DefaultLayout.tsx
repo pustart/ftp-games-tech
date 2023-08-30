0846a6a98cc54cc9d134b3f56e08dd7ec06e82b9
pustart <polaris9955@gmail.com>
2023-08-30 18:14:00 +0300

    fix: add styles for background gradient

--- src/layouts/Default/DefaultLayout.tsx
+++ src/layouts/Default/DefaultLayout.tsx
@@ -6,12 +6,12 @@ import { DefaultLayoutProps } from './DefaultLayout.props';
 import styles from "./DefaultLayout.module.scss"
 
 function DefaultLayout({ children, fancyBackground }: DefaultLayoutProps): JSX.Element {
+  const bgColor: string = fancyBackground ? "radial-gradient(55.85% 58.02% at 62.94% 12.65%, rgba(66, 20, 159, 0.36) 0%, rgba(0, 0, 0, 0.00) 48.46%), radial-gradient(57.48% 56.51% at 37.29% 6.42%, rgba(20, 29, 111, 0.64) 0.92%, rgba(0, 0, 0, 0.00) 59.80%)" : "black";
+
   return (
-    <div className={styles["default-wrapper"]}>
+    <div className={styles["default-wrapper"]} style={{ background: bgColor }}>
       {
         fancyBackground ? (<>
-        <Box zIndex="-1" w="112.5rem" h="124rem" position="fixed" left="-34.3rem" top="-51.8rem" bg="radial-gradient(70.71% 70.71% at 50% 50%, rgba(20, 29, 111, 0.64) 0%, rgba(0, 0, 0, 0.00) 60.00%)" />
-        <Box zIndex="-1" w="87.5rem" h="96rem" position="fixed" right="-23.7rem" top="-22.8rem" bg="radial-gradient(70.71% 70.71% at 50% 50%, rgba(66, 20, 159, 0.30) 0%, rgba(0, 0, 0, 0.00) 60.00%)" />
         </>) : null
       }
       <Header/>
